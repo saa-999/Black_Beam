39eb3f7ca9680e275d22620ee45064724fcf9e18
saud <sa3od34ki@gmail.com>
2026-08-25 18:11:11 +0300

    feat: add struct Product

---
+++ BlackBeam.Services.Inventory/Models/Product.cs
@@ -0,0 +1,12 @@
+namespace BlackBeam.Services.Inventory.Models;
+public class Product
+{
+    public Guid Id {get; set;} = Guid.NewGuid();
+    public string Name  {get; set;} = string.Empty;
+    public string Barcode  {get; set;} = string.Empty;
+    public decimal Price   {get; set;} 
+    public int StockQuantity { get; set; }
+    public bool IsActive    { get; set; } = true;
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
+}
