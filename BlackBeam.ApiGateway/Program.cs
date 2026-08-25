4a5d9c56401e84c8ca737f7b25cf1887e96ad1d3
saud <sa3od34ki@gmail.com>
2026-08-25 13:24:56 +0300

    feat(api-gateway): configure YARP with centralized auth, rate limiting, and CORS
    - setup YARP reverse proxy for identity and orders routing
    - implement centralized JWT Bearer authentication to protect internal microservices
    - enforce Fixed Window Rate Limiting on login endpoints to prevent brute-force attacks
    - configure global CORS policy to enable frontend integration

--- BlackBeam.ApiGateway/Program.cs
+++ BlackBeam.ApiGateway/Program.cs
@@ -1,17 +1,64 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+using Microsoft.AspNetCore.RateLimiting;
+using DotNetEnv;
+using System.Text.Json;
+
+Env.Load();
+
 var builder = WebApplication.CreateBuilder(args);
+
+string? jwtSecret = builder.Configuration["JwtSettings:Secret"];
+string? jwtIssuer = builder.Configuration["JwtSettings:Issuer"];
+if (string.IsNullOrEmpty(jwtSecret) || string.IsNullOrEmpty(jwtIssuer))
+{
+     throw new InvalidOperationException("المتغيرات فراغة في ملف البيئة !!!");
+}
 
 builder.Services.AddReverseProxy().LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
 
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddCors(opt => 
+{
+    opt.AddPolicy("AllowAll", b => b 
+     .AllowAnyOrigin()
+    .AllowAnyMethod()
+    .AllowAnyHeader()); 
+});
+
+builder.Services.AddRateLimiter(options =>
+{
+   options.AddFixedWindowLimiter("LoginPolicy", opt => 
+   {
+      opt.Window = TimeSpan.FromMinutes(1);
+      opt.PermitLimit = 5;  
+   });
+});
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuerSigningKey = true,
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtSecret)), 
+            ValidateIssuer = true,
+            ValidIssuer = jwtIssuer,
+            ValidateAudience = false 
+        };
+    });
+
+builder.Services.AddAuthorization(options =>
+{
+    options.AddPolicy("RequireAuth", policy => policy.RequireAuthenticatedUser());
+});
 
 var app = builder.Build();
 
-if (builder.Environment.IsDevelopment())
-{
-    app.MapSwagger();
-    app.MapSwaggerUI();
-}
+app.UseCors("AllowAll");
+app.UseRateLimiter();
+app.UseAuthentication();
+app.UseAuthorization();
 
 app.MapReverseProxy();
 
